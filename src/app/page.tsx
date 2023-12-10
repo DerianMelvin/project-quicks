e781bf4375d1a0ac5f39947c43d6321500a9d916
Derian Melvin <derianmelvin88@gmail.com>
2023-12-10 22:41:22 +0700

    create initial menu button
    - create component MenuButton.tsx
    - create types\components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import MenuButton from "@/components/MenuButton";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hello World</h1>
+    <main className="w-full h-full px-[34px] py-[27px] flex items-end justify-end">
+      <button onClick={() => console.log("Menu clicked")}>
+        <MenuButton size={68} />
+      </button>
     </main>
   );
 }
